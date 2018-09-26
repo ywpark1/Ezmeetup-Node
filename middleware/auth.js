656ab409ad7ffac8ef752e4996c608fe5ea97050
Yeonwoo Park <ywoo1234@gmail.com>
2018-09-26 09:38:17 -0400

    Change verify to callback style

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,14 @@ const config = require('config');
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
   if (!token) return res.status(401).send('Access denied. No token provided.');
+  
 
-  try {
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    req.user = decoded; 
-    next();
-  }
-  catch (ex) {
-    res.status(400).send('Invalid token.');
-  }
+  jwt.verify(token, config.get('jwtPrivateKey'), (error, decoded) => {
+      if(error) {
+        res.status(400).send('Invalid token.');
+      }
+
+      req.user = decoded; 
+      next();
+  });
 }
