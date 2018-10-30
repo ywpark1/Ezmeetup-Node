92f3f650f60c72c621f1809335d897b4af0fbdca
Yeonwoo Park <ywoo1234@gmail.com>
2018-10-30 08:44:39 -0400

    Add GetAllEventsWithCategories, findAllWithCategories functions

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -2,10 +2,12 @@ const _ = require("lodash");
 // const jwt = require('jsonwebtoken');
 const db = require("../startup/dbconnection");
 // const config = require('config');
+const Op = require("sequelize").Op;
 const Event = db.events;
 const Category = db.categories;
 const EventCategory = db.eventCategories;
 const User = db.users;
+const UserCategory = db.userCategories;
 
 function getOneEventWithCategories(eventId) {
   return Event.findOne({
@@ -25,9 +27,26 @@ function getOneEventWithCategories(eventId) {
   });
 }
 
+function getAllEventsWithCategories() {
+  return Event.findAll({
+    include: [
+      {
+        model: EventCategory,
+        attributes: ["categoryId"],
+        include: [
+          {
+            model: Category,
+            attributes: ["categoryName"]
+          }
+        ]
+      }
+    ]
+  });
+}
+
 // Display list of all Events.
 exports.findAll = (req, res) => {
-  Event.findAll()
+  getAllEventsWithCategories()
     .then(events => {
       res.send(events);
     })
@@ -36,21 +55,72 @@ exports.findAll = (req, res) => {
     });
 };
 
+// Display list of all Events based on User Categories.
+exports.findAllWithCategories = (req, res) => {
+  UserCategory.findAll({
+    where: { userId: req.params.userId },
+    attributes: ["categoryId"]
+  })
+    .then(categories => {
+      const categoryArr = categories.map(a => a.categoryId);
+      return EventCategory.findAll({
+        where: { categoryId: { [Op.in]: categoryArr } },
+        distinct: true
+        // group: "eventId"
+      });
+    })
+    .then(eventCats => {
+      const eventArr = eventCats.map(a => a.eventId);
+
+      return Event.findAll({
+        where: { id: { [Op.in]: eventArr } },
+        include: [
+          {
+            model: EventCategory,
+            attributes: ["categoryId"],
+            include: [
+              {
+                model: Category,
+                attributes: ["categoryName"]
+              }
+            ]
+          }
+        ],
+        distinct: true
+      });
+      //   res.send(eventArr);
+    })
+    .then(events => {
+      res.send(events);
+    })
+    .catch(err => {
+      res.status(400).send(err.errors[0].message);
+    });
+  //   res.send(userCategoriesArr);
+  //   Event.findAll()
+  //     .then(events => {
+  //       res.send(events);
+  //     })
+  //     .catch(err => {
+  //       res.status(400).send(err.errors[0].message);
+  //     });
+};
+
 // Create new event
 exports.create = (req, res) => {
   Event.create({
-    // eventName: req.body.eventName,
-    // eventLocation: req.body.eventLocation,
-    // eventDescription: req.body.eventDescription,
-    // eventCapacity: req.body.eventCapacity,
-    // eventDate: req.body.eventDate,
-    // userId: req.body.userId
-    eventName: "Test Event 1",
-    eventLocation: "Toronto, ON",
-    eventDescription: "This is Test Event Description.",
-    eventCapacity: 15,
-    eventDate: "2018-11-12",
-    userId: 1
+    eventName: req.body.eventName,
+    eventLocation: req.body.eventLocation,
+    eventDescription: req.body.eventDescription,
+    eventCapacity: req.body.eventCapacity,
+    eventDate: req.body.eventDate,
+    userId: req.body.userId
+    // eventName: "Test Event 1",
+    // eventLocation: "Toronto, ON",
+    // eventDescription: "This is Test Event Description.",
+    // eventCapacity: 15,
+    // eventDate: "2018-11-12",
+    // userId: 1
   })
     .then(event => {
       const categoryArr = req.body.categoryIds.map(id => ({
