88580179b4bb022bdb64bdc1dd8d66a34be3b42d
Yeonwoo Park <ywoo1234@gmail.com>
2018-10-30 08:46:18 -0400

    Update home routes

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,29 @@
-'use strict';
+"use strict";
 
-const router = require('express').Router();
-const { authJwt } = require('../middleware/auth');
+const router = require("express").Router();
+const { authJwt } = require("../middleware/auth");
 
+const fs = require("fs");
+const path = require("path");
+const showdown = require("showdown");
+const converter = new showdown.Converter();
 
-router.get('/', (req, res) => {
-    res.send('This is PRJ666 EzMeetup Frontpage.');
-    // res.render('index', { title: 'My Express App', message: 'Hello' });
+var text = fs.readFileSync(path.resolve(__dirname, "../README.md")).toString();
+var html = converter.makeHtml(text);
+
+router.get("/", (req, res) => {
+  //   res.send("This is PRJ666 EzMeetup Frontpage.");
+  res.send("Chat server is running");
+//   res.send(html);
 });
 
-router.get('/hello', authJwt, (req, res) => {
-    res.send('This is a private route!!!!');
-});
+// router.get('/hello', authJwt, (req, res) => {
+//     res.send('This is a private route!!!!');
+// });
 
-router.get('/:version', (req, res) => {
-    res.send('This is PRJ666 EzMeetup API version ' + req.params.version + '!');
-    // res.render('index', { title: 'My Express App', message: 'Hello' });
-});
-
-
+// router.get('/:version', (req, res) => {
+//     res.send('This is PRJ666 EzMeetup API version ' + req.params.version + '!');
+//     // res.render('index', { title: 'My Express App', message: 'Hello' });
+// });
 
 module.exports = router;
