131ca67ab398817d7df58bc3de99ab7ffd07bb15
Yeonwoo Park <ywoo1234@gmail.com>
2018-11-26 23:54:10 -0500

    Convert Markdown to html

--- routes/home.js
+++ routes/home.js
@@ -12,18 +12,13 @@ var text = fs.readFileSync(path.resolve(__dirname, "../README.md")).toString();
 var html = converter.makeHtml(text);
 
 router.get("/", (req, res) => {
-  //   res.send("This is PRJ666 EzMeetup Frontpage.");
-  res.send("Chat server is running");
-//   res.send(html);
+  //     res.send("This is PRJ666 EzMeetup Frontpage.");
+  //   res.send("Chat server is running");
+  res.send(html);
 });
 
-// router.get('/hello', authJwt, (req, res) => {
-//     res.send('This is a private route!!!!');
-// });
-
-// router.get('/:version', (req, res) => {
-//     res.send('This is PRJ666 EzMeetup API version ' + req.params.version + '!');
-//     // res.render('index', { title: 'My Express App', message: 'Hello' });
+// router.get("/forgotPassword", (req, res) => {
+//   res.render("index", { title: "My Express App", message: "Hello" });
 // });
 
 module.exports = router;
