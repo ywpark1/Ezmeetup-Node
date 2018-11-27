438de96e32860a19310082357f43086ab2d5cd37
Yeonwoo Park <ywoo1234@gmail.com>
2018-11-26 23:55:27 -0500

    Enable public directory access

--- startup/prod.js
+++ startup/prod.js
@@ -1,15 +1,16 @@
-const helmet = require('helmet');
-const compression = require('compression');
-const passport = require('passport');
+const helmet = require("helmet");
+const compression = require("compression");
+const passport = require("passport");
 
 module.exports = function(express, app) {
-    app.set('view engine', 'pug'); // Template Engine(use with render)
-    app.set('views', './views'); // all pug files are located for views
+  app.set("view engine", "pug"); // Template Engine(use with render)
+  app.set("views", "./views"); // all pug files are located for views
 
-    app.use(helmet());
-    app.use(compression());
-    app.use(express.urlencoded({ extended: true })); //key=value&key=value
-    app.use(express.static('public'));
+  app.use(helmet());
+  app.use(compression());
+  app.use(express.urlencoded({ extended: true })); //key=value&key=value
+  // app.use(express.static('public'));
+  app.use("/public", express.static("public"));
 
-    app.use(passport.initialize());
-}
+  app.use(passport.initialize());
+};
